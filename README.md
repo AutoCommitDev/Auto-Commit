d1c6db3cd6aaef0cafe940fd2be2725e2eaa677a
LautyDev <86755961+LautyDev@users.noreply.github.com>
2024-01-04 02:49:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+<h1 align="center">Auto Commit</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/AutoCommitDev/Auto-Commit-Code?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/AutoCommitDev/Auto-Commit-Code?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/AutoCommitDev/Auto-Commit-Code?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/AutoCommitDev/Auto-Commit-Code?color=56BEB8">
+</p>
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="https://github.com/AutoCommitDev" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About
+
+Automated TypeScript script commits to a GitHub repo every 24 hours, updating a 'README.md' file with the day's number. Utilizes 'simple-git' for Git interactions and GitHub updates.
+
+## :rocket: Technologies
+
+The following tools were used in this project:
+
+- [Node.js](https://nodejs.org/en/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## :white_check_mark: Requirements
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Node](https://nodejs.org/en/) installed.
+
+## :checkered_flag: Starting
+
+```bash
+# Clone this project
+$ git clone https://github.com/AutoCommitDev/Auto-Commit-Code
+
+# Access
+$ cd Auto-Commit-Code
+
+# Install dependencies
+$ npm i
+
+# Run the project
+$ npm run start
+```
+
+Made with :heart: by <a href="https://github.com/LautyDev" target="_blank">LautyDev</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import simpleGit, { SimpleGit } from "simple-git";
+import fs from "fs";
+import "dotenv/config";
+
+const repositoryPath = process.cwd();
+const filePath = "./README.md";
+
+async function performCommitAndPush(): Promise<void> {
+  const git: SimpleGit = simpleGit(repositoryPath);
+
+  const date = new Date();
+  const currentDay = date.getDate();
+  const quantityDays = Math.floor(
+    (new Date(date.getFullYear() + 1, 0, 0).getTime() -
+      new Date(date.getFullYear(), 0, 0).getTime()) /
+      (1000 * 60 * 60 * 24)
+  );
+  const amountCommits = (await git.log()).total;
+
+  const content: string = `# Day ${currentDay} - ${date.getFullYear()}\n- Remaining days: ${
+    quantityDays - currentDay
+  }/${quantityDays}\n- Amount of commits: ${
+    amountCommits + 1
+  }\n\n<img src="https://raw.githubusercontent.com/AutoCommitDev/AutoCommitDev/output/snake.svg" alt="Snake animation"/>\n\nAuto Commit - Made by [LautyDev](https://github.com/LautyDev)`;
+
+  fs.writeFileSync(filePath, content);
+
+  await git.add(filePath);
+
+  const commitMessage: string = `📅 Automated commit: Day ${currentDay}.`;
+  await git.commit(commitMessage);
+
+  console.log(`Commit was made: ${commitMessage}.`);
+
+  async function sendMessageDiscord(message: string) {
+    await fetch(process.env.DISCORD_WEBHOOK as string, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: message,
+      }),
+    });
+  }
+
+  await git.push("origin", "main").catch(async (error: any) => {
+    return await sendMessageDiscord(
+      `An error occurred while pushing.\n\`\`\`${error}\`\`\``
+    );
+  });
+
+  console.log("The remote repository was pushed.");
+
+  await sendMessageDiscord("**Commit was made:** ${commitMessage}.");
+}
+
+function wait24Hours(): Promise<void> {
+  const millisecondsInADay: number = 24 * 60 * 60 * 1000;
+  return new Promise((resolve) => setTimeout(resolve, millisecondsInADay));
+}
+
+async function runScript() {
+  while (true) {
+    await performCommitAndPush();
+    await wait24Hours();
+  }
+}
+
+runScript().catch((error: Error) => console.error(error));
